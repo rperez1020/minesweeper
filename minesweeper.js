e8d7d741c21de9d58bf52787e5f69add21a4346b
Raymond <rtperez1020@gmail.com>
2024-02-23 16:08:52 -0800

    update

---
+++ minesweeper.js
@@ -0,0 +1,17 @@
+// file dedicated to game logic
+
+export function createBoard(boardSize, numberOfMines){
+    const board = []
+    for(let x = 0; x < boardSize; x++){
+        const row = []
+        for(let y = 0; y < boardSize; y++){
+            const tile = {
+                x,
+                y,
+            }
+            row.push(tile)
+        }
+        board.push(row)
+    }
+    return board
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+// dedicated to game board / UI
+import { createBoard } from "./minesweeper.js"
+
+console.log(createBoard(2,2))
+
+
+// populate the game board with tiles and mines
+// left click on tiles to reveal
+// right click on tiles to mark mines
+// check win condition 
+// 8:21
