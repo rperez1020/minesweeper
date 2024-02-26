d26fe8881aed0244f78ab5950c5e3b5e8312abb7
Raymond <rtperez1020@gmail.com>
2024-02-25 21:00:48 -0800

    update

--- script.js
+++ script.js
@@ -1,11 +1,70 @@
 // dedicated to game board / UI
-import { createBoard } from "./minesweeper.js"
-
-console.log(createBoard(2,2))
+import { createBoard, markTile, TILE_STATUSES, revealTile, checkWin, checkLose} from "./minesweeper.js"
 
 
 // populate the game board with tiles and mines
-// left click on tiles to reveal
-// right click on tiles to mark mines
-// check win condition 
-// 8:21
+const BOARD_SIZE = 5
+const NUMBER_OF_MINES = 2
+
+const board = createBoard(BOARD_SIZE,NUMBER_OF_MINES)
+const boardElement = document.querySelector('.board')
+const minesLeftText = document.querySelector('[data-mine-count')
+const messageText = document.querySelector('.subtext')
+
+board.forEach(row => {
+    row.forEach(tile => {
+        boardElement.append(tile.element)
+        tile.element.addEventListener('click', ()=>{
+            revealTile(board, tile)
+            checkGameEnd()
+        })
+        tile.element.addEventListener('contextmenu', e => {
+            e.preventDefault()
+            markTile(tile)
+            listMinesLeft()
+        })
+    })
+})
+
+boardElement.style.setProperty('--size', BOARD_SIZE)
+minesLeftText.textContent = NUMBER_OF_MINES
+
+
+function listMinesLeft(){
+    const markedTilesCount = board.reduce((count, row)=>{
+        return (count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length)
+    },0)
+
+    minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount
+}
+
+
+
+function checkGameEnd(){
+    const win = checkWin(board)
+    const lose = checkLose(board)
+
+    if(win || lose){
+        boardElement.addEventListener('click', stopProp, {capture:true})
+        boardElement.addEventListener('contextMenu', stopProp, {capture:true})
+    }
+
+    if(win){
+        messageText.textContent = 'You Win'
+    }
+    if(lose){
+        messageText.textContent = 'You Lose'
+        board.forEach(row => {
+            row.forEach(tile => {
+                if(tile.TILE_STATUSES === TILE_STATUSES.MARKED) markTile(tile)
+                if(tile.mine) revealTile(board, tile)
+            })
+        })
+    }
+}
+
+
+function stopProp(e){
+    e.stopImmediatePropagation()
+}
+
